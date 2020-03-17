7ea596e4c144b6770b231d4768e7e45a4e030abc
Konstantin Komelin <konstantin.komelin@gmail.com>
2020-03-17 19:34:10 +0300

    Added main wrappers for browser and node.

---
+++ index.js
@@ -0,0 +1,9 @@
+function r(m){return m && m.default || m;}
+module.exports = global.DOMPurify = global.DOMPurify || (
+	typeof process === 'undefined' ? r(require('dompurify')) : (function() {
+    const DOMPurifyInitializer = r(require('dompurify'));
+    const { JSDOM } = r(require('jsdom'));
+    const { window } = new JSDOM('<!DOCTYPE html>');
+    return DOMPurifyInitializer(window);
+	})
+);
