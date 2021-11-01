eba1c03e964b618807d3d3e8106f6ddaf434de2f
Konstantin Komelin <konstantin.komelin@gmail.com>
2021-11-01 21:01:16 +0300

    Extended automated tests (refs #91)

--- test.js
+++ test.js
@@ -2,10 +2,16 @@
 
 const { sanitize } = require('./index');
 
-const dirty = `<a href="javascript:alert('danger')">A link</a>`;
-
-test('DOMPurify sanitizes HTML', () => {
+test('DOMPurify removes unwanted JS from HTML anchor', () => {
+  const dirty = `<a href="javascript:alert('danger')">A link</a>`;
   const factual = sanitize(dirty);
   const expected = `<a>A link</a>`;
+  expect(factual).toBe(expected);
+});
+
+test('DOMPurify removes unwanted JS from HTML image', () => {
+  const dirty = `<img onload="alert(1)" />`
+  const factual = sanitize(dirty);
+  const expected = `<img>`;
   expect(factual).toBe(expected);
 });
