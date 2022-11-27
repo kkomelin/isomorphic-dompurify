70cf2bc7e402e4f36e409d4a966ea8a6a5cb45b1
1Copenut <pierce.trevor@gmail.com>
2022-11-27 17:04:56 -0600

    Adding a check for existence of the window object.

--- browser.js
+++ browser.js
@@ -1,1 +1,5 @@
-module.exports = window.DOMPurify || (window.DOMPurify = require('dompurify').default || require('dompurify'));
+if (typeof window !== "undefined") {
+  module.exports =
+    window.DOMPurify ||
+    (window.DOMPurify = require("dompurify").default || require("dompurify"));
+}
