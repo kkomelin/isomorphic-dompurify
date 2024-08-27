6ab7256f57572c8ec3499b9b761853b797a6d974
Konstantin Komelin <konstantin.komelin@gmail.com>
2024-08-27 08:33:17 +0200

    Switched from Jest to Vitest for testing.

--- tests/sanitize.test.js
+++ tests/sanitize.test.js
@@ -1,16 +1,15 @@
-'use strict';
+import { expect, test } from "vitest";
+import { sanitize } from "../index";
 
-const { sanitize } = require('./index');
-
-test('DOMPurify removes unwanted JS from HTML anchor', () => {
+test("DOMPurify removes unwanted JS from HTML anchor", () => {
   const dirty = `<a href="javascript:alert('danger')">A link</a>`;
   const factual = sanitize(dirty);
   const expected = `<a>A link</a>`;
   expect(factual).toBe(expected);
 });
 
-test('DOMPurify removes unwanted JS from HTML image', () => {
-  const dirty = `<img onload="alert(1)" />`
+test("DOMPurify removes unwanted JS from HTML image", () => {
+  const dirty = `<img onload="alert(1)" />`;
   const factual = sanitize(dirty);
   const expected = `<img>`;
   expect(factual).toBe(expected);
